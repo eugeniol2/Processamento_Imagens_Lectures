a2560bae5cca5af64343471c18ad966f6690cff5
eugenio eugenio <eugeniodornelesl2@gmail.com>
2026-09-05 19:52:39 -0300

    feat: Q1 results done

---
+++ Q1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+array = np.array([False, True, False, True, False, False, True, False, False, False])
+
+def convertToNumber(array):
+  return array.astype(int)
+
+def getArrayMinusLastIndex(array):
+  return convertToNumber(array[:-1])
+
+def getArrayMinusFirstIndex(array):
+  return convertToNumber(array[1:])
+
+def getQ1Results(array):
+  subtractionResults = getArrayMinusLastIndex(array) - getArrayMinusFirstIndex(array)
+  return np.sum(subtractionResults < 0)
+
+Result = getQ1Results(array)
+
+print(Result)
