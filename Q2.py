312fe1093637af9718e7c21b726e8b99fac89c30
eugenio eugenio <eugeniodornelesl2@gmail.com>
2026-09-05 20:19:58 -0300

    Q2 done

---
+++ Q2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+npArray = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+
+def getOddNumbersArray(array):
+  return array[array % 2 != 0].astype(int)
+
+def getArrayToThePowerOf(array, powerOf):
+  return array ** powerOf
+
+oddArray = getOddNumbersArray(npArray)
+
+result = getArrayToThePowerOf(oddArray, 2)
+
+print(result)
