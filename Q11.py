8001a1efbb1fedcb80ba2db3605208ad9d4f18c7
eugenio eugenio <eugeniodornelesl2@gmail.com>
2026-09-06 03:05:52 -0300

    Q11 done

---
+++ Q11.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def changeArrayValuesLessBy(array, oldValue, newValue):
+  array[array < oldValue] = newValue
+  return array
+
+def get1DArraySum(array):
+  return np.sum(array)
+
+v = np. array ([1 , -2, 3, 4, -5, 6, -7, 8, -9])
+
+onlyPositiveArray = changeArrayValuesLessBy(v, 0, 0)
+
+result = get1DArraySum(onlyPositiveArray)
+print(result)
