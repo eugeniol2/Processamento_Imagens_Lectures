4d0d15fa3ae1ad4d30771012ce8c69658141e7e5
eugenio eugenio <eugeniodornelesl2@gmail.com>
2026-09-06 12:41:21 -0300

    Q14 done

---
+++ Q14.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+rng = np.random.default_rng(42)
+
+A = rng.integers(low=1, high=2, size=(10, 10))
+B = rng.integers(low=1, high=2, size=(10, 10))
+
+def sumTwoMatrices(mA, mB):
+  return mA + mB
+
+def getAbsoluteDiff(mA, mB):
+  return np.abs(mA - mB)
+
+def getMean(matrice):
+  return np.mean(matrice)
+
+def getBothMean(mA, mB):
+  return getMean(mA), getMean(mB)
+
+def newMatriceWithHighestValues(mA, mB):
+  return np.maximum(mA, mB)
+
+
+matricesSum = sumTwoMatrices(A, B)
+matricesDiff = getAbsoluteDiff(A, B)
+matricesMean = getBothMean(A, B)
+maximumNewMatrice = newMatriceWithHighestValues(A, B)
+
+print('A',matricesSum)
+print('B',matricesDiff)
+print('C',matricesMean)
+print('D',maximumNewMatrice)
