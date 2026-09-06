d1c3a43d330b261f43d026d9302bda660f494608
eugenio eugenio <eugeniodornelesl2@gmail.com>
2026-09-06 01:41:20 -0300

    Q5 done

---
+++ Q5.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = np.array([-3, -2, -1, 0, 1, 2, 3])
+
+def changeArrayValuesLessBy(array, oldValue, newValue):
+  array[array < oldValue] = newValue
+  return array
+
+result = changeArrayValuesLessBy(x, 0, 0)
+print(result)
